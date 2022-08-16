138126d291b8cdc3e30a5bceb085427df0e6d5bf
APURVA SINGH <singhapurva42@gmail.com>
2022-08-16 12:39:27 +0530

    Learnt Callback Hell and Inversion of Control

--- callbackHell.js
+++ callbackHell.js
@@ -1,7 +1,27 @@
-console.log("Welcome");
+// console.log("Welcome");
 
-setTimeout(function () {
-    console.log("Back");
-}, 4000);
+// setTimeout(function () {
+//     console.log("Back");
+// }, 4000);
 
-console.log("Apurva");
+// console.log("Apurva");
+
+/* E-Commerce Example */
+
+const cart = ["jacket", "shoes", "earrings", "jeans"];
+
+api.createOrder(cart, function () {
+    api.proceedToPayment(function () {
+        api.showOrderSummary(function () {
+            api.updateWallet()
+        })
+    })
+})
+
+//This structure of code in which one callback is inside the other is known as callback hell
+
+/* Inversion of Control Example */
+api.createOrder(cart, function () {
+    api.proceedToPayment()
+})
+
